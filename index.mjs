3a3cb3680a8a746b6fc1be22f3f2d1f405990655
vijay <vijaymano0501@gmail.com>
2024-09-08 02:35:48 +0530

    finally

--- index.mjs
+++ index.mjs
@@ -547,6 +547,6 @@ app.get('/file/:contractId', async (req, res) => {
 });
 
 
-app.listen(process.env.port, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
